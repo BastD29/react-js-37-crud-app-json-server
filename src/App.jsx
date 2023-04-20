b46e843359dbf0680e36ab3c10f6c2fd23d8bd28
Bastien Daniel <bastien.daniel@rawfish.com>
2023-04-20 15:00:37 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MyForm from "./pages/MyForm";
+
+export default function App() {
+  return (
+    <>
+      <MyForm />
+    </>
+  );
+}
---
+++ src/pages/MyForm.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+function MyForm() {
+  const [formData, setFormData] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const id = uuidv4();
+    const newData = { ...formData, id };
+    try {
+      const response = await fetch("http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newData),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" name="name" onChange={handleInputChange} />
+      </label>
+      <label>
+        Email:
+        <input type="email" name="email" onChange={handleInputChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default MyForm;
